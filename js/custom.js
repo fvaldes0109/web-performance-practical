aab60df7998d71d8a402059f8cb6936b074dd228
fvaldes0109 <fvaldes0109@gmail.com>
2024-03-22 11:29:24 +0100

    Add location banner

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,21 @@
     });
     
   })(window.jQuery);
+
+  
+const load = () => {
+    
+  const geoText = document.querySelector('.geo-text');
+
+  if (geoText && navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&localityLanguage=en`)
+        .then(response => response.json())
+        .then(data => {
+          geoText.textContent = 'Hey! We also deliver to ' + data.city;
+        });
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', load);
