d1f077dfce9d81a76ce953a51b29aaeecbe2617d
fvaldes0109 <fvaldes0109@gmail.com>
2024-03-25 11:08:14 +0100

    Add express compression

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 
 const app = express();
 
+app.use(compression());
 app.use(express.static('dist'));
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
