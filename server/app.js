d15fb02354c28cb9bf3a24a8bf7892fe1228272c
fvaldes0109 <fvaldes0109@gmail.com>
2024-03-25 11:34:48 +0100

    Add endpoint for reservations

--- server/app.js
+++ server/app.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(compression());
 app.use(express.static('dist', ['etag']));
 
+app.post('/reservation', (req, res) => {
+  res.send('Reservation request received!');
+});
+
 app.listen(3000, () => console.log('Server is running on port 3000'));
