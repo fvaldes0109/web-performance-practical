6379d37cd5571b45e464059008f1688933887e62
fvaldes0109 <fvaldes0109@gmail.com>
2024-03-25 11:12:38 +0100

    Add etag generation

--- server/app.js
+++ server/app.js
@@ -4,6 +4,6 @@ const compression = require('compression');
 const app = express();
 
 app.use(compression());
-app.use(express.static('dist'));
+app.use(express.static('dist', ['etag']));
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
